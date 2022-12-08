46955789857b3e62fd00b739a1f6058c29cf7872
NotBuilded <119298704+NotBuilded@users.noreply.github.com>
2022-12-08 08:26:51 +0100

    Update FIGL.lua

--- FIGL.lua
+++ FIGL.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-		local Window = OrionLib:MakeWindow({Name = "FIGB / Glaze Community", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
+		local Window = OrionLib:MakeWindow({Name = ""..game.Name.."", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
 	
 	
 		local Tab = Window:MakeTab({
